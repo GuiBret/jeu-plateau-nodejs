5be77d5c9fc9ccd0de28f87f5aea2381953b2517
GuiBret <guillaume.bretzner@gmail.com>
2018-03-01 14:25:12 +0100

    design(): now using Bootstrap for single-button modals

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,19 +1,15 @@
 
 /* Creates a modal with Material which will require only one button (access to restricted page, game finished, etc) */
 function createModalDialog(info, callback) {
-    let dialog = document.createElement("dialog");
-
-    dialog.classList = "md1-dialog";
-
-    dialog.innerHTML = `<h4 class='md1-dialog__title text-center'>${info.title}</h4><div class='md1-dialog__content'><p>${info.content}</p></div><div class="md1-dialog__actions text-center"><button type='button' class="mdl-button mdl-js-button mdl-button--raised back">OK</button></div>`;
-
-    document.body.appendChild(dialog);
-
-    $("button.back").on("click", callback);
+    let dialog = $(`<div class='modal fade' id='modalDialog'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='title'>${info.title}</h5></div><div class='modal-body'>${info.content}</div><div class='modal-footer mx-auto'><button type="button" class="btn btn-primary" id='btn-ok'>OK</button></div></div></div></div>`);
 
 
-    dialog.showModal();
-    $(".mdl-button").blur();
+    $("body").append(dialog);
+
+    $("#btn-ok").on("click", callback);
+
+
+    dialog.modal("show");
 
 }
 // Creates a modal with Material which will require 2 buttons (for example, surrender request)
