ecb5e6a7f088fcad03505528fc10bd6715a3e38a
GuiBret <guillaume.bretzner@gmail.com>
2018-03-01 15:24:02 +0100

    test(): fixed tests

--- spec/database.spec.js
+++ spec/database.spec.js
@@ -4,50 +4,50 @@ const DatabaseConnection = require("../node_modules/jeu-backend/utils/database_c
 describe("database connect", function() {
    beforeAll(function() {
        this.db_connection = new DatabaseConnection();
-   }); 
-    
+   });
+
     it("should properly connect to the user and get its data", function(done) {
-        
+
         let connectionData = {"username": "usertest", "password": "monjeuplateau"};
-        
+
         this.db_connection.getUserData(connectionData).then( (res)=> {
-           
-            expect(res.id).toEqual(1);   
+
+            expect(res.id).toEqual(1);
             done();
         });
 
     });
-    
-    
+
+
     it("should handle an incorrect password", function(done) {
-        
+
         let connectionData = {"username": "usertest", "password": "monjeudeplateau"}; // Deliberately wrong password
-        
+
         this.db_connection.getUserData(connectionData).then( (res)=> {
-           
-            expect(res).toEqual("WRONG_PASSWORD"); // The function will return 1 if the user is not in the database   
+
+            expect(res).toEqual("WRONG_PASSWORD"); // The function will return 1 if the user is not in the database
             done();
         }).catch((err) => {
             console.info("Erreur");
 
         });
     });
-    
+
 });
 
 describe("user management", function() {
-    
+
     beforeAll(function() {
            this.db_connection = new DatabaseConnection();
        });
-    
-    
+
+
     it("should perform a proper user creation", function(done) {
-        
+
         let params = {"username": "usertest3", "password": "monmotdepasse"};
-        
+
         this.db_connection.createUser(params).then((res) => {
-            
+
             expect(res.username).toEqual("usertest3");
             this.db_connection.removeUser(params["username"]); // We remove the user when it's done in order to be able to retest it later
             done();
@@ -55,13 +55,13 @@ describe("user management", function() {
 
             done();
         });
-        
-        
+
+
     });
-    
+
     it("should be able to detect the fact that a user tries to enter a username which is already in the DB", function(done) {
        let params = {"username": "usertest", "password": "monmotdepasse"};
-        
+
         this.db_connection.createUser(params).then((res) => {
             expect(res.username).toEqual("usertest");
             this.db_connection.removeUser(params["username"]); // We remove the user when it's done in order to be able to retest it later
@@ -71,8 +71,8 @@ describe("user management", function() {
             done();
 
         });
-        
-        
+
+
     });
 
 });
@@ -81,8 +81,8 @@ describe("match data", function() {
     beforeAll(function() {
            this.db_connection = new DatabaseConnection();
     });
-    
-    
+
+
     it("should be able to get the user's games", function(done) {
         let matches = this.db_connection.getUserMatches(1); // We"re looking for the matches of one of the dummy users
         this.db_connection.getUserMatches(1).then((res) => {
@@ -90,35 +90,35 @@ describe("match data", function() {
             done();
         }).catch((err) => {
             console.log(err);
-            
+
         });
-        
-        
+
+
     });
-    
+
     it("should be able to include a match", function(done) {
        let match_info = {"winner": 1, "loser": 2, "type":"online", "remaining_hp": 25};
-        
+
         this.db_connection.addMatch(match_info).then((res) => {
-           expect(res).toEqual("SUCCESSFULLY_ADDED"); 
+           expect(res).toEqual("SUCCESSFULLY_ADDED");
             this.db_connection.removeLastMatch();
             done();
         })
         .catch((err) => {
-          console.log(err);  
+          console.log(err);
             done();
         });
-        
-        
+
+
     });
-    
+
 });
 
 describe("getOptions", function() {
     beforeAll(function() {
         this.db_connection = new DatabaseConnection();
     });
-    
+
     it("should able to get the options (set as default) for the first test user", function(done) {
         this.db_connection.getOptions(1).then((res) => {
             expect(res.volume).toEqual(.5);
@@ -134,18 +134,18 @@ describe("setOptions", function() {
     beforeAll(function() {
         this.db_connection = new DatabaseConnection();
     });
-    
+
     it("should be able to correctly modify the specified user's options", function(done) {
        this.db_connection.setOptions({"joueur_concerne": 1, "volume": 0.7, "animations": 0, "language": "fr"}).then(() => { // Then, after we've modified the options, we check if they have been updated in the database
            this.db_connection.getOptions(1).then((options) => {
                expect(options.animations).toEqual(0);
                expect(options.volume).toEqual(0.7);
-               
+
                done();
            });
        });
     });
-    
+
     afterAll(function(done) {
         this.db_connection.setOptions({"joueur_concerne": 1, "volume": .5, "animations": 1}).then(() => {
             done();
@@ -154,32 +154,41 @@ describe("setOptions", function() {
 });
 
 describe("createOptions", function() {
-   beforeAll(function(done) { // First, we have to create a new user (which will be deleted in the afterAll)
+   beforeEach(function(done) { // First, we have to create a new user (which will be deleted in the afterAll)
        this.db_connection = new DatabaseConnection();
-       
-       this.db_connection.createUser({"username": "utilisateurdetest", "password": "monutilisateurdetest"}).then((userInfo) => {
+
+       this.db_connection.createUser({"username": "utilisateurdetest3", "password": "monutilisateurdetest"}).then((userInfo) => {
            this.user_id = userInfo.id; // Needed as a parameter of the createOptions function
+           this.language = "fr";
+
            done();
-           
+
+       }).catch((err) => {
+         console.log(err);
        });
    });
-    
+
     it("should correctly insert the options of a newly-created user", function(done) {
-        this.db_connection.createOptions(this.user_id).then(() => {
+        this.db_connection.createOptions(this.user_id, this.language).then(() => {
+
             // Then, we check the options of this user
             this.db_connection.getOptions(this.user_id).then((options) => {
                 expect(options.joueur_concerne).toEqual(this.user_id);
+                expect(options.language).toEqual(this.language);
                 done();
             })
-             
+
         });
     });
-    
-    
-    afterAll(function() {
-        this.db_connection.removeLastOptions("utilisateurdetest");
-        this.db_connection.removeUser("utilisateurdetest");
-        
+
+
+    afterEach(function(done) {
+        this.db_connection.removeLastOptions("utilisateurdetest3").then(() => {
+            this.db_connection.removeUser("utilisateurdetest3").then(() => {
+              done();
+            })
+        });
+
     })
 });
 
@@ -189,16 +198,16 @@ describe("generateNewMatchQuery", function() {
        this.db_connection = new DatabaseConnection();
        this.game_info = {"winner": 1, "loser": 2, "type": "online", "remaining_hp": 50};
    });
-    
+
     it("should return the correct request with specified info", function() {
-        
+
         let query = this.db_connection.generateNewMatchQuery(this.game_info);
         expect(query).toEqual(`INSERT INTO matches VALUES(
-                '', 
-                1, 
-                2, 
+                '',
+                1,
+                2,
                 (select id from match_type where match_label="online"),
-                50, 
+                50,
                 NOW())`);
     });
 });
@@ -208,15 +217,15 @@ describe("generateConnectionQuery", function() {
        this.db_connection = new DatabaseConnection();
        this.params = {"username": "monutilisateurtest"};
    });
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateConnectionQuery(this.params);
-        
-        expect(query).toEqual(`select 
+
+        expect(query).toEqual(`select
             connexion.username, connexion.password, connexion.id, options.animations, options.volume, options.language
-            from options 
-            left join connexion 
-            on options.joueur_concerne = connexion.id 
+            from options
+            left join connexion
+            on options.joueur_concerne = connexion.id
             where connexion.username="monutilisateurtest";`);
     })
 });
@@ -225,20 +234,20 @@ describe("generateUserCreationQuery", function() {
     beforeAll(function() {
         this.db_connection = new DatabaseConnection();
         this.userInfo = {"username": "monutilisateurtest", "password": "monpasswordtest"};
-        
+
     })
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateUserCreationQuery(this.userInfo);
-    
+
         expect(query).toEqual(`INSERT INTO connexion VALUES (
-                    '', 
-                    'monutilisateurtest', 
-                    'monpasswordtest', 
+                    '',
+                    'monutilisateurtest',
+                    'monpasswordtest',
                     '');`
-        );    
+        );
     });
-    
+
 });
 
 describe("generateUserMatchesQuery", function() {
@@ -246,29 +255,29 @@ describe("generateUserMatchesQuery", function() {
         this.db_connection = new DatabaseConnection();
         this.id = 25;
     })
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateUserMatchesQuery(this.id);
-    
-        expect(query).toEqual(`SELECT * FROM matches WHERE 25 in (winner, loser)`);    
+
+        expect(query).toEqual(`SELECT * FROM matches WHERE 25 in (winner, loser)`);
     });
-    
+
 });
 
 describe("generateGetUserId", function() {
     beforeAll(function() {
         this.db_connection = new DatabaseConnection();
         this.userInfo = {"username": "monutilisateurtest"};
-        
+
     })
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateGetUserIdQuery(this.userInfo);
-    
+
         expect(query).toEqual(`SELECT id from connexion where username="monutilisateurtest";`
-        );    
+        );
     });
-    
+
 });
 
 
@@ -276,36 +285,36 @@ describe("generateUserProfileQuery", function() {
     beforeAll(function() {
         this.db_connection = new DatabaseConnection();
         this.id = 25;
-        
+
     })
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateUserProfileQuery(this.id);
-    
-        expect(query).toEqual(`select username, 
+
+        expect(query).toEqual(`select username,
                 (select count(*) as online_wins from matches where type=2 and 25 in (winner, loser)) as online_matches,
                 (select count(*) from matches where winner=25 and type=2) as online_wins,
                 (select count(*) from matches where type=3 and 25 in (winner, loser)) as ai_matches,
                 (select count(*) from matches where winner=25 and type=3) as ai_wins
                 from connexion where id=25;`
-        );    
+        );
     });
-    
+
 })
 
 describe("generateGetOptionsQuery", function() {
     beforeAll(function() {
         this.db_connection = new DatabaseConnection();
         this.id = 25;
-        
+
     })
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateGetOptionsQuery(this.id);
-    
-        expect(query).toEqual(`SELECT * from options where joueur_concerne=25;`);    
+
+        expect(query).toEqual(`SELECT * from options where joueur_concerne=25;`);
     });
-    
+
 });
 
 describe("generateSetOptionsQuery", function() {
@@ -313,13 +322,13 @@ describe("generateSetOptionsQuery", function() {
         this.db_connection = new DatabaseConnection();
         this.info = {"animations": 1, "volume": 0.5, "joueur_concerne": 25, "language": "fr"};
     })
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateSetOptionsQuery(this.info);
-    
-        expect(query).toEqual(`UPDATE options set animations=1, volume=0.5, language="fr" where joueur_concerne=25`);    
+
+        expect(query).toEqual(`UPDATE options set animations=1, volume=0.5, language="fr" where joueur_concerne=25`);
     });
-    
+
 });
 
 describe("generateCreateOptionsQuery", function() {
@@ -328,11 +337,11 @@ describe("generateCreateOptionsQuery", function() {
         this.id = 25;
         this.language = "en"
     })
-    
+
     it("should return the correct request with specified data", function() {
         let query = this.db_connection.generateCreateOptionsQuery(this.id, this.language);
-    
-        expect(query).toEqual(`INSERT INTO options VALUES ('', 25, 1, .5, "en")`);    
+
+        expect(query).toEqual(`INSERT INTO options VALUES ('', 25, 1, .5, "en")`);
     });
-    
+
 });
--- spec/gamesocketmanager.spec.js
+++ spec/gamesocketmanager.spec.js
@@ -13,37 +13,37 @@ let express = require('express'),
 describe("enterRoom", function() {
 
     beforeAll(function() {
-        
+
         let socket = { // Fake socket class
             emit: function(msg) {
-            
+
             },
             join: function() {
-            
+
             },
             on: function() {
-            
+
             }
         }
-        
+
         this.gh = gh;
         this.game_id = gh.createGame();
         this.gsm = new GameSocketManager(socket, DBConnection, io, this.gh);
-        
-        
+
+
     });
-    
+
     it("should have triggered the join function", function(done) {
         spyOn(this.gsm.socket, "join");
-        
+
         this.gsm.enterRoom(this.game_id).then(() => {
             expect(this.gsm.socket.join).toHaveBeenCalledWith(`room-game${this.game_id}`);
             done();
         });
-        
-        
+
+
     });
-    
+
     afterAll(function(done) {
         gh.deleteGame(this.game_id);
         done();
@@ -52,45 +52,43 @@ describe("enterRoom", function() {
 
 /* PBs online tests : requires 2 real sockets */
 describe("onlineGameConfirmation", function() {
-    
+
 });
 
 describe("localGameConfirmation", function() {
     beforeAll(function() {
-        
+
         let socket = { // Fake socket class
-            emit: function(msg) {
-            
+            emit: function(msg, args) {
             },
             join: function() {
-            
+
             },
             on: function() {
-            
+
             }
         }
-        
+        spyOn(socket, "emit").and.callThrough();
         this.gh = gh;
         this.game_id = gh.createGame();
         this.gsm = new GameSocketManager(socket, DBConnection, io, this.gh);
-        
-        
+
+
     });
-    
+
    it("should have emitted envoiGrille with the grid content", function() {
-       spyOn(this.gsm.socket, "emit");
 
        this.gsm.localGameConfirmation({"id": this.game_id, "noms": ["machin", "truc"]}).then(() => {
            expect(this.gsm.socket.emit).toHaveBeenCalledWith("envoiGrille", {"grille": jasmine.any(Object)});
        });
-       
-       
+
+
    });
-    
+
     it('should contain the correct ID', function() {
         expect(this.gsm.socket.gameID).toEqual(this.game_id);
     });
-    
+
     afterAll(function(done) {
         this.gsm.gh.deleteGame(this.game_id);
         done();
@@ -101,40 +99,40 @@ describe("usernameManagement", function() {
     beforeAll(function() {
         let socket = { // Fake socket class
             emit: function(msg) {
-            
+
             },
             join: function() {
-            
+
             },
             on: function() {
-            
+
             }
         }
-        
+
         this.gh = gh;
         this.game_id = gh.createGame();
         this.gsm = new GameSocketManager(socket, DBConnection, io, this.gh);
-        
+
     });
-    
+
     it("should have emitted envoiNomVersClient to the room with correct settings", function(done) {
-        
+
         let params = {"nom": ["machin", "truc"], "id": 1};
         this.gsm.socket.gameID = this.game_id;
-        
-        
+
+
         this.gsm.gh.getGame(this.game_id).then((game) => {
             game.setParams(params["nom"], true, this.gsm.socket);
             spyOn(this.gsm.io.in(`room-game${this.game_id}`), "emit");
             this.gsm.usernameManagement(params).then(() => {
-                
+
                 expect(this.gsm.io.in(`room-game${this.game_id}`).emit).toHaveBeenCalledWith("envoiNomVersClient", {"nom": ["machin", "truc"], "id": 1});
                 done();
-            });    
+            });
         });
-        
+
     });
-    
+
     afterAll(function() {
         this.gsm.gh.deleteGame(this.game_id);
     })
@@ -144,41 +142,41 @@ describe("launchFirstTurn", function() {
     beforeAll(function() {
         let socket = { // Fake socket class
             emit: function(msg) {
-            
+
             },
             join: function() {
-            
+
             },
             on: function() {
-            
+
             }
         }
-        
+
         this.gh = gh;
         this.game_id = gh.createGame();
         this.gsm = new GameSocketManager(socket, DBConnection, io, this.gh);
-        
+
     });
-    
+
     it('should emit lancementTour (as local) with correct settings', function(done) {
         let params = {"nom": ["machin", "truc"], "id": 1};
         this.gsm.socket.gameID = this.game_id;
-        
-        
+
+
         this.gsm.gh.getGame(this.game_id).then((game) => {
             game.setParams(params["nom"], true, this.gsm.socket);
-            
+
             this.gsm.usernameManagement(params).then(() => {
                 spyOn(this.gsm.socket, "emit");
                 this.gsm.launchFirstTurn().then(() => {
                     expect(this.gsm.socket.emit).toHaveBeenCalledWith("lancementTour", {"joueur_actuel": 0, "combatLance": false, "dep_possibles": game.getPossibleMoves(0), "ancienne_arme": -1});
                     done();
-                    
+
                 });
-            });    
+            });
         });
     });
-    
+
     afterAll(function(done) {
         this.gsm.gh.deleteGame(this.game_id);
         done();
@@ -189,38 +187,38 @@ describe("requestMovement", function() {
     beforeAll(function() {
         let socket = { // Fake socket class
             emit: function(msg) {
-            
+
             },
             join: function() {
-            
+
             },
             on: function() {
-            
+
             }
         }
-        
+
         this.gh = gh;
         this.game_id = gh.createGame();
         this.gsm = new GameSocketManager(socket, DBConnection, io, this.gh);
-        
+
     });
-    
+
    it("should emit confirmationDeplacement with correct settings", function(done) {
        let params = {"nom": ["machin", "truc"], "id": 1};
         this.gsm.socket.gameID = this.game_id;
-        
-        
+
+
         this.gsm.gh.getGame(this.game_id).then((game) => {
             game.setParams(params["nom"], true, this.gsm.socket);
-            
+
             this.gsm.usernameManagement(params).then(() => {
-    
+
                 this.gsm.launchFirstTurn().then(() => {
                     spyOn(this.gsm.io.in(`room-game${this.game_id}`), "emit");
                     let position = game.getCurrentPlayer().position,
                         new_position,
                         dep_possibles = game.grille.calculDepDispos(position);
-                    
+
                     if(game.grille.elemDansArray(dep_possibles, [position[0] + 1, position[1]])) { // Si le joueur peut aller à droite
                         new_position = [position[0] + 1, position[1]];
                     } else if (game.grille.elemDansArray(dep_possibles, [position[0], position[1 + 1]]))  { // Si le joueur peut aller en bas
@@ -230,23 +228,23 @@ describe("requestMovement", function() {
                     } else { // S'il peut aller en haut (on va supposer qu'il ne sera pas bloqué)
                         new_position = [position[0], position[1] - 1];
                     }
-                    
-                    
+
+
                     this.gsm.requestMovement(new_position).then(() => {
                         expect(this.gsm.io.in(`room-game${this.game_id}`).emit).toHaveBeenCalledWith("confirmationDeplacement", {"position": new_position, "prev_position": position, "id_arme":-1, grille:game.getCurrentGrid(), cur_player: jasmine.any(Object) });
-                    
-                        done();             
-                        
+
+                        done();
+
                     });
-                    
-                    
-                    
+
+
+
                 });
-            });    
+            });
         });
-       
+
    });
-    
+
     afterAll(function(done) {
         this.gsm.gh.deleteGame(this.game_id);
         done();
@@ -258,41 +256,41 @@ describe("surrenderOffline", function() {
     beforeEach(function() {
         let socket = { // Fake socket class
             emit: function(msg) {
-            
+
             },
             join: function() {
-            
+
             },
             on: function() {
-            
+
             }
         }
-        
+
         this.gh = gh;
         this.game_id = gh.createGame();
         socket.gameID = this.game_id;
         this.gsm = new GameSocketManager(socket, DBConnection, io, this.gh);
-        
+
     });
-    
+
     it("should have deleted the socket", function() {
        this.gsm.surrenderOffline();
-        
-       expect(this.gsm.socket).toBeNull(); 
+
+       expect(this.gsm.socket).toBeNull();
     });
-    
+
     it("should have deleted the game", function(done) {
-        
+
        this.gsm.surrenderOffline();
-        
+
         this.gsm.gh.getGame(this.game_id).catch((error) => {
             expect(error).toEqual("NO_GAME_FOUND");
             done();
         });
-       
+
     });
-    
-    
+
+
     afterAll(function() {
         this.gsm = null;
         this.gh = null;
@@ -303,77 +301,77 @@ describe("surrenderOnline", function() {
     beforeEach(function(done) {
         let socket = { // Fake socket class
             emit: function(msg) {
-            
+
             },
             join: function() {
-            
+
             },
             on: function() {
-            
+
             }
         },
             db_connection = {
                 addMatch: function(match_info) {
-                    
+
                 }
             }
-        
+
         this.gh = gh;
         this.game_id = this.gh.createGame();
         //console.log(this.gh.games);
         socket.gameID = this.game_id;
         this.socket = socket;
         this.gsm = new GameSocketManager(socket, db_connection, io, this.gh);
-        
+
         this.gh.getGame(this.socket.gameID).then((game) => {
-           
-            
+
+
             let info_p1 = {"nom": "usertest", "id": 0, "online_id": 1},
                 info_p2 = {"nom": "usertest", "id": 1, "online_id": 2};
-            
+
             game.setParams(["",""], false, socket);
-            
+
             game.ajouterInfosJoueur(info_p1);
             game.ajouterInfosJoueur(info_p2);
-            
+
             done();
-            
-            
+
+
         });
-        
-        
+
+
     });
-    
+
     it("should have deleted the socket", function() {
-        
+
         this.gsm.surrenderOnline(this).then(() => {
-            expect(this.gsm.socket).toBeNull();    
+            expect(this.gsm.socket).toBeNull();
         });
-        
-        
+
+
     });
-    
+
     it("should have deleted the game", function(done) {
-        
+
         this.gsm.surrenderOnline(this).then(() => {
-        
+
             this.gh.getGame(this.game_id).catch((error) => {
                 //console.log(error);
                 expect(error).toEqual("NO_GAME_FOUND");
                 done();
-            }); 
-            
+            });
+
         }).catch(() => {
-            console.log("Erreur");  
+            console.log("Erreur");
         });
-        
+
         //expect(1).toEqual(1);
-        
+
     });
-    
+
     it("should have called 'addMatch' in order to add the match to the database", function(done) {
         let socket = this.socket;
-        
+
         spyOn(this.gsm.DBConnection, "addMatch");
         this.gsm.surrenderOnline(this).then(() => {
             expect(this.gsm.DBConnection.addMatch).toHaveBeenCalled();
