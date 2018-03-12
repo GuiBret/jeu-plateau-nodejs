d4664568467d5a8cd669343fff8534fc6fd70656
GuiBret <guillaume.bretzner@gmail.com>
2018-03-12 12:24:26 +0100

    refactor(): removed useless lines

--- public/js/interface/main-menu.js
+++ public/js/interface/main-menu.js
@@ -1,41 +1,40 @@
 var socket = io.connect((window.location.hostname === "localhost") ? "http://localhost:5000" : `http://${window.location.hostname}:5000`);
 
 $("document").ready(function() {
-    
+
     verifClientConnecte(); // On vérifie si le client est connecté normalement ou en tant qu'invité, si aucun des deux, on affiche un dialog et le redirige vers la page de connexion
-    
-    console.log(__.getLocale());
-    
+
+
     /* Button tooltips requiring connection */
     $("#btn-options, #btn-profile").on("mouseover", function() {
         $(this).tooltip();
     })
-    
+
     $("#btn-profile").tooltip();
-    
-    
+
+
     /* Button click management*/
     $(".button_container button").on("click", function(e) { // Listener unique pour tous les boutons, un switch dans le callback pour savoir quel bouton a été cliqué
 
        switch($(e.target).attr('id')) {
            case "btn-1v1-online": // 1v1 en ligne
                socket.emit("demandePartieOnline");
-               
+
                $("#btn-1v1-online").html("Recherche en cours...");
                $("#btn-1v1-online").attr("disabled", "true");
                socket.on("onlineEnemyFound", function(params) {
-                  window.location.replace(`../game/online?id=${ params["id"] }`); 
+                  window.location.replace(`../game/online?id=${ params["id"] }`);
                });
             break;
            case "btn-ia": // Duel contre l'IA
-               
+
             break;
             case "btn-1v1-local": // 1v1 en local
                socket.emit("demandePartieLocale");
                socket.on("partieLocaleTrouvee", function(params) {
-                  window.location.replace(`../game/local?id=${ params["id"] }`); 
+                  window.location.replace(`../game/local?id=${ params["id"] }`);
                });
-               
+
             break;
             case "btn-options": // Bouton des options
                window.location.replace(`../options/`);
@@ -48,7 +47,7 @@ $("document").ready(function() {
             break;
            default:
             break;
-       } 
+       }
     });
 });
 
@@ -67,7 +66,7 @@ function handleDisconnect() {
     sessionStorage.removeItem("id");
     sessionStorage.removeItem("username");
     sessionStorage.removeItem("password");
-    
-    
+
+
     window.location.replace("../");
 }
--- public/js/translator.js
+++ public/js/translator.js
@@ -1,31 +1,26 @@
 i18n.configure({
     locales: {
         "en": {
-            "Machin": "truc",
             "vient d'être créé avec succès": "has been successfully created.",
             "Le compte": "The account",
             "Invité": "Guest",
             // Local game
             "Entrez le nom du joueur 1 : ": "Enter the name for player 1 : ",
             "Entrez le nom du joueur 2 : ": "Enter the name for player 2 : ",
-            
+
             "Attaquer": "Attack",
             "Défendre": "Defend",
             "Revolver": "Revolver",
             "Lance-roquette": "Rocket Launcher",
             "Fusil à pompe": "Shotgun",
             "Fusil": "Rifle"
-            
+
         },
         "fr": {
-            "Machin": "truc"
-        }
-        
-    },
-    
-    globalize:true,
-    cookie:sessionStorage.getItem("language")
-    
-});
 
-i18n.setLocale(sessionStorage.getItem("language")); // Reloads the language for each page (maybe should have used a cookie that I would have put in configure, but easier like that (and doesn't work with sessionStorage))
+        }
+
+    },
+
+    globalize:true,
+});
